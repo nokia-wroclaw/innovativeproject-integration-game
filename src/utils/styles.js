a0431ba743e01a76c0b6b93ad0c4c5b63b4646ff
MartynaStrzempek <martyna.strzempek@gmail.com>
2018-03-07 23:31:28 +0100

    React starter

---
+++ src/utils/styles.js
@@ -0,0 +1,9 @@
+//Color palette
+export const colors = {
+    textColor: '#212121',
+};
+
+export const typography = {
+    mainFamily: '\'Roboto\', \'Arial\', sans-serif',
+    mainWeight: 300,
+};
---
+++ src/views/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container } from '../../utils/styledComponents';
+
+class App extends Component {
+  render() {
+    return (
+        <Container></Container>
+    );
+  }
+}
+
+export default App;
