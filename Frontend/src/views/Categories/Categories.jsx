b61d31ce6c81fb14c7229737a82758f035b50e41
Ala <tomczakala@wp.pl>
2018-03-11 21:38:13 +0100

    f-categories

--- Frontend/src/views/Categories/Categories.jsx
+++ Frontend/src/views/Categories/Categories.jsx
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
 import { Container } from '../../utils/styledComponents';
+import { Wrapper } from './Categories_styles';
+
+import { Accordion, Icon } from 'semantic-ui-react'
+
+
 
 class Categories extends Component {
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+  const { index } = titleProps
+  const { activeIndex } = this.state
+  const newIndex = activeIndex === index ? -1 : index
+
+  this.setState({ activeIndex: newIndex })
+  }
+
     render() {
+    const { activeIndex } = this.state
         return (
             <Container>
+                <Wrapper>
                 Categories
+                    <Accordion styled>
+                      <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+                       <Icon name='dropdown' />
+                       Actors
+                       </Accordion.Title>
+                       <Accordion.Content active={activeIndex === 0}>
+                       <p>
+                       Brad Pitt
+                       </p>
+                       <p>
+                       Leonardo DiCaprio
+                       </p>
+                       <p>
+                       Al Pacino
+                       </p>
+                      </Accordion.Content>
+
+                      <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+                       <Icon name='dropdown' />
+                       Sportsmen
+                       </Accordion.Title>
+                       <Accordion.Content active={activeIndex === 1}>
+                       <p>
+                       Justyna Kowalczyk
+                       </p>
+                       <p>
+                       Adam Małysz
+                       </p>
+                       <p>
+                       Kamil Stoch
+                       </p>
+                       </Accordion.Content>
+
+                      </Accordion>
+                </Wrapper>
+
+
             </Container>
-        );
-    }
-}
+
+
+                       );
+            }
+ }
 
 export default Categories;
+
--- Frontend/src/views/Categories/Categories_styles.js
+++ Frontend/src/views/Categories/Categories_styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
-export const Div = styled.div`
-  
+export const Div = styled.div
+
+export const Wrapper = styled.div`
+    width: 400%;
+    float: left
+    margin-left: 20px;
+
 `;
+
+  
