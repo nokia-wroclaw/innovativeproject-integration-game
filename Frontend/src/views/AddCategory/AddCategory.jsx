ac6e6474bda7b7b502db9779150f79e31c531212
Ala <tomczakala@wp.pl>
2018-03-12 15:25:26 +0100

    pola dodające kategorie i postacie

--- Frontend/src/views/AddCategory/AddCategory.jsx
+++ Frontend/src/views/AddCategory/AddCategory.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import { Container } from '../../utils/styledComponents';
-import { Input, Form } from 'semantic-ui-react'
+import { Input, Form, Button } from 'semantic-ui-react'
+import { Wrapper1 } from './AddCategory_styles';
+
+
 
 class AddCategory extends Component {
     render() {
         return (
             <Container>
+            <Wrapper1>
                 Add Categories & Charakters
+                <p>
+                You can add all informations about chcarakters
+                </p>
                 <Form>
                     <Form.Group widths='equal'>
                     <Form.Input fluid id='form-subcomponent-shorthand-input-categories' label='Categories' placeholder='Categories' />
-                    <Form.Input fluid id='form-subcomponent-shorthand-input-charakters' label='Charakters' placeholder='Charakters' />
                     </Form.Group>
+                    <Form.Group widths='equal'>
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-charakters-first-name' label='Charakters First Name' placeholder='Charakters First Name' />
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-charakters-last-name' label='Charakters Last Name' placeholder='Charakters Last Name' />
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-nick-name' label='Nick Name' placeholder='Nick Name' />
+                    </Form.Group>
+                 <Button type='Add'>Add</Button>
                 </Form>
+            </Wrapper1>
             </Container>
         );
     }
--- Frontend/src/views/AddCategory/AddCategory_styles.js
+++ Frontend/src/views/AddCategory/AddCategory_styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Div = styled.div`
+export const Div = styled.div
+
+export const Wrapper1 = styled.div`
+    width: 100%;
+    float: left
+    margin-left: 20px;
+
   
 `;
