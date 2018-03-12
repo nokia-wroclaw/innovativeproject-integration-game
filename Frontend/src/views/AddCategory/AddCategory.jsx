98b3a8ae339048954448891338b9b67bdbf3acd4
Ala <tomczakala@wp.pl>
2018-03-12 14:42:39 +0100

    Początkowy widok strony

--- Frontend/src/views/AddCategory/AddCategory.jsx
+++ Frontend/src/views/AddCategory/AddCategory.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Container } from '../../utils/styledComponents';
+import { Input, Form } from 'semantic-ui-react'
 
 class AddCategory extends Component {
     render() {
         return (
             <Container>
-                Add
+                Add Categories & Charakters
+                <Form>
+                    <Form.Group widths='equal'>
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-categories' label='Categories' placeholder='Categories' />
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-charakters' label='Charakters' placeholder='Charakters' />
+                    </Form.Group>
+                </Form>
             </Container>
         );
     }
