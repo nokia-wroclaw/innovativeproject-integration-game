32baeff7a3bc80b1293ecc69d0e177074665010d
Ala <tomczakala@wp.pl>
2018-03-14 22:44:37 +0100

    View Page - categori

--- Frontend/src/views/Categories/Categories_styles.js
+++ Frontend/src/views/Categories/Categories_styles.js
@@ -3,10 +3,21 @@ import styled from 'styled-components';
 export const Div = styled.div
 
 export const Wrapper = styled.div`
-    width: 400%;
+    width: 100%;
     float: left
-    margin-left: 40px;
+    margin-top: 0px;
+    background: '#212121';
+    padding: 2em 2em;
 
-`;
+`
+
+export const TitlePage2 = styled.div`
+  padding: 1em 30em;
+  margin: 0em;
+  background: papayawhip;
+  color: black;
+  
+`
+;
 
   
