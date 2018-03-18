993f143128164ff39afd472088e57a7d94644698
Ala <tomczakala@wp.pl>
2018-03-18 17:56:43 +0100

    Underline Title

--- Frontend/src/views/AddCategory/AddCategory.jsx
+++ Frontend/src/views/AddCategory/AddCategory.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import { Container } from '../../utils/styledComponents';
+import { Form, TextArea } from 'semantic-ui-react'
+import { Wrapper1 } from './AddCategory_styles';
+import { TitlePage1 } from './AddCategory_styles';
+import { Button1 } from './AddCategory_styles';
+
+
+
 
 class AddCategory extends Component {
     render() {
         return (
             <Container>
-                Add
+            <Wrapper1>
+            <TitlePage1>
+                Add Categories & Charakters
+            </TitlePage1>
+                <Form>
+                    <Form.Group widths='equal'>
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-categories' label='Categories' placeholder='Categories' />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-charakters-first-name' label='Charakters First Name' placeholder='Charakters First Name' />
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-charakters-last-name' label='Charakters Last Name' placeholder='Charakters Last Name' />
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-nick-name' label='Nick Name' placeholder='Nick Name' />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                    <Form.Input fluid id='form-subcomponent-shorthand-input-description' control={TextArea} label='Description' placeholder='Description' />
+                    </Form.Group>
+
+                 <Button1 type='Add'>Add</Button1>
+                 <Button1 class='ui icon plus icon button'><i class='plus icon'></i></Button1>
+
+
+                </Form>
+            </Wrapper1>
             </Container>
         );
     }
--- Frontend/src/views/AddCategory/AddCategory_styles.js
+++ Frontend/src/views/AddCategory/AddCategory_styles.js
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
 
-export const Div = styled.div`
+export const Div = styled.div
 
-`;
+export const Wrapper1 = styled.div`
+    width: 100%;
+    float: left
+    margin-top: 0px;
+    background: '#212121';
+    padding: 2em 2em;
+`
+export const TitlePage1 = styled.div`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 20px;
+    color: black;
+    margin-bottom:35px
+    font-family: "Roboto Slab", serif;
+    border-bottom: solid 5px #f56a6a;
+    padding: 20px;
+
+`
+
+export const Button1 = styled.button`
+    background-color: transparent;
+    border-radius: 0.375em;
+    border: 0;
+    box-shadow: inset 0 0 0 2px #f56a6a;
+    color: #f56a6a !important;
+    cursor: pointer;
+    display: inline-block;
+    font-family: "Roboto Slab", serif;
+    font-size: 13px;
+    font-weight: 700;
+    height: 4em;
+    letter-spacing: 0.075em;
+    line-height: 3em;
+    margin-right: 20px;
+    margin-top:20px
+    padding: 0 3em;
+    text-align: center;
+    text-decoration: none;
+    text-transform: uppercase;
+    white-space: nowrap;
+}
+`
+;
