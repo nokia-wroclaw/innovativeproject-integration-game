cb107e8696d3d150940b0c9c8740d69c143bb215
krzysiu26911 <227086@student.pwr.edu.pl>
2018-03-19 20:22:25 +0100

    categories

--- Frontend/src/views/Categories/Categories.jsx
+++ Frontend/src/views/Categories/Categories.jsx
@@ -2,74 +2,48 @@ import React, { Component } from 'react';
 import { Container } from '../../utils/styledComponents';
 import { Wrapper } from './Categories_styles';
 import { TitlePage2 } from './Categories_styles';
+import { Accordion } from 'semantic-ui-react'
+import AccordionElement from '../../components/AccordionElement/AccordionElement';
+import data from './data.json';
+
+export default class Categories extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: data
+        };
+    }
+
+    renderCategories() {
+        return (
+
+            this.state.data.map((rowdata,index)=>
+                <div>
+                    {
+                        <AccordionElement activeIndex={index} index={index} name={rowdata.category} names={rowdata.people.map((subRowData,k)=>
+                            {return   subRowData.name + " " + subRowData.surname+ " " + ((subRowData.nickname==null)?("\n"):(subRowData.nickname + "\n"))}
+                        )}/>
 
 
-import { Accordion, Icon } from 'semantic-ui-react'
-
-
-
-class Categories extends Component {
-  state = { activeIndex: 0 }
-
-  handleClick = (e, titleProps) => {
-  const { index } = titleProps
-  const { activeIndex } = this.state
-  const newIndex = activeIndex === index ? -1 : index
-
-  this.setState({ activeIndex: newIndex })
-  }
-
+                    }
+                </div>)
+        )
+    }
+    
     render() {
-    const { activeIndex } = this.state
         return (
             <Container>
                 <Wrapper>
-                <TitlePage2>
-                Categories
-                </TitlePage2>
                     <Accordion styled>
-                      <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
-                       <Icon name='dropdown' />
-                       Actors
-                       </Accordion.Title>
-                       <Accordion.Content active={activeIndex === 0}>
-                       <p>
-                       Brad Pitt
-                       </p>
-                       <p>
-                       Leonardo DiCaprio
-                       </p>
-                       <p>
-                       Al Pacino
-                       </p>
-                      </Accordion.Content>
-
-                      <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
-                       <Icon name='dropdown' />
-                       Sportsmen
-                       </Accordion.Title>
-                       <Accordion.Content active={activeIndex === 1}>
-                       <p>
-                       Justyna Kowalczyk
-                       </p>
-                       <p>
-                       Adam Małysz
-                       </p>
-                       <p>
-                       Kamil Stoch
-                       </p>
-                       </Accordion.Content>
-
-                      </Accordion>
+                        <TitlePage2>
+                            Categories
+                        </TitlePage2>
+                        {this.renderCategories()}
+                    </Accordion>
                 </Wrapper>
-
-
             </Container>
-
-
-                       );
-            }
- }
-
-export default Categories;
-
+        );
+    }
+}
