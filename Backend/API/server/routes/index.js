71e20cbb9472ca5542fbc2c5fb488ca25d981339
emlk0404 <36955409+emlk0404@users.noreply.github.com>
2018-04-10 15:51:30 +0200

    Add files via upload

---
+++ Backend/API/server/routes/index.js
@@ -0,0 +1,23 @@
+const categoriesController = require('../controllers').categories;
+const peopleController = require('../controllers').people;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to API!',
+  }));
+
+  app.post('/api/categories', categoriesController.create);
+  app.get('/api/categories', categoriesController.list);
+  app.get('/api/categories/:categoryId', categoriesController.retrieve);
+  app.put('/api/categories/:categoryId', categoriesController.update);
+  app.delete('/api/categories/:categoryId', categoriesController.destroy);
+/*
+  app.post('/api/categories/:categoryId/people', peopleController.create);
+  app.put('/api/categories/:categoryId/people/:peopleId', peopleController.update);
+  app.delete(
+    '/api/categories/:categoryId/people/:peopleId', peopleController.destroy
+  );
+  app.all('/api/categories/:categoryId/people', (req, res) => res.status(405).send({
+    message: 'Method Not Allowed',
+  }));*/
+};
