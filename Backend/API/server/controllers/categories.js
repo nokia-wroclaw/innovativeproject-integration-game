c0a6927d0f80934f67cfa2cfd5432e063910b9dd
emlk0404 <36955409+emlk0404@users.noreply.github.com>
2018-04-17 22:58:58 +0200

    Add files via upload

---
+++ Backend/API/server/controllers/categories.js
@@ -0,0 +1,92 @@
+const Category = require('../models').category;
+const People = require('../models').people;
+
+
+module.exports = {
+  create(req, res) {
+    
+    return Category
+      .create({
+        
+          name: req.body.name,
+      })
+      .then((category) => res.status(201).send(category))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  list(req, res) {
+    return Category
+      .findAll({
+        include: [{
+          model: People,
+          as: 'people',
+        }],
+        order: [
+          ['createdAt', 'DESC'],
+          [{ model: People, as: 'people' }, 'createdAt', 'ASC'],
+        ],
+      })
+      .then((categories) => res.status(200).send(categories))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  retrieve(req, res) {
+    return Category
+      .findById(req.params.categoryId, {
+        include: [{
+          model: People,
+          as: 'people',
+        }],
+      })
+      .then((category) => {
+        if (!category) {
+          return res.status(404).send({
+            message: 'Category Not Found',
+          });
+        }
+        return res.status(200).send(category);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Category
+      .findById(req.params.categoryId, {
+        include: [{
+          model: People,
+          as: 'people',
+        }],
+      })
+      .then(category => {
+        if (!category) {
+          return res.status(404).send({
+            message: 'Category Not Found',
+          });
+        }
+        return category
+          .update({
+            name: req.body.name || category.name,
+          })
+          .then(() => res.status(200).send(category))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return Category
+      .findById(req.params.categoryId)
+      .then(category => {
+        if (!category) {
+          return res.status(400).send({
+            message: 'Category Not Found',
+          });
+        }
+        return category
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+};
