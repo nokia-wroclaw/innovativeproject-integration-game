da3b9823f2ec8d057bf63ff884b49704ea859382
emlk0404 <36955409+emlk0404@users.noreply.github.com>
2018-04-17 23:00:18 +0200

    Delete category.js

--- Backend/API/server/models/category.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  var category = sequelize.define('category', {
-    name: DataTypes.STRING
-  }, {});
-  category.associate = models => {
-    category.belongsToMany(models.people, {through: 'peopleCategory'});
-  };
-  return category;
-};
