3266bfab10eb30f9ec3ff9f7e9659af0a76bb2bf
Damodar Łepski <damodar9999@gmail.com>
2018-04-19 01:20:54 +0200

    Mobile: - main game loop WIP (problems with communication between fragments)

--- Mobile/app/src/main/java/pwr/damodarlepski/integrationgame/GameActivity.kt
+++ Mobile/app/src/main/java/pwr/damodarlepski/integrationgame/GameActivity.kt
@@ -1,12 +1,9 @@
 package pwr.damodarlepski.integrationgame
 
-import android.content.Intent
-import android.nfc.Tag
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Toast
 
+/*
 var teamOneCounter = 0
 var teamTwoCounter = 0
 
@@ -17,21 +14,22 @@ var ArrayCategory = ArrayCategoryData.toMutableList()
 var ArrayPeople = ArrayPeopleData.toMutableList()
 
 var indexOfRound = 0
-val ArrayRounds = arrayOf("Description \n In round 1, the Cluegiver draws a name card and begins giving clues to his teammates (Guessers). The Cluegiver can sing, hum, point, charade, give full descriptions of the person, etc."
-        , "One word \n In round 2, the Cluegiver may give only one word for each clue, which may be repeated as often as desired. Once the word is said, only non-verbal clues (sound effects, charades) may be given."
-        , "Showing without speaking \n In round 3, the Cluegiver may not use any words at all. Only sounds and charades may be used for each clue. "
-        , "Pose \n In round 4, the Guesser starts with their eyes closed while the Cluegiver freezes in a pose depicting their clue. Once frozen, the Guesser opens their eyes and must give one guess.")
+val ArrayRounds = arrayOf("Description\nIn round 1, the Cluegiver draws a name card and begins giving clues to his teammates (Guessers). The Cluegiver can sing, hum, point, charade, give full descriptions of the person, etc."
+        , "One word \nIn round 2, the Cluegiver may give only one word for each clue, which may be repeated as often as desired. Once the word is said, only non-verbal clues (sound effects, charades) may be given."
+        , "Showing without speaking \nIn round 3, the Cluegiver may not use any words at all. Only sounds and charades may be used for each clue. "
+        , "Pose \nIn round 4, the Guesser starts with their eyes closed while the Cluegiver freezes in a pose depicting their clue. Once frozen, the Guesser opens their eyes and must give one guess.")
 
 var team_name = ""
+*/
 
 class GameActivity : AppCompatActivity() {
     private val manager = supportFragmentManager
 
-    val gameMechanics = GameMechanics()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val gameMechanics = GameMechanics()
 
         gameMechanics.addCard("Sport", "Robert Lewandowski", "")
         gameMechanics.addCard("Actor", "Cezary Pazura", "")
@@ -39,15 +37,22 @@ class GameActivity : AppCompatActivity() {
         gameMechanics.addCard("Dance", "Katarzyna Cichopek", "")
         gameMechanics.addCard("Writer", "Andrzej Sapkowski", "")
 
+/*
         teamOneCounter = 0
         teamTwoCounter = 0
         ArrayCategory = ArrayCategoryData.toMutableList()
         ArrayPeople = ArrayPeopleData.toMutableList()
         indexOfRound = 0
         team_name = ""
+*/
 
         val transaction = manager.beginTransaction()
         val fragment = RoundFragment()
+
+        val passBundle = Bundle()
+        passBundle.putSerializable("game_mechanics", gameMechanics)
+        fragment.arguments = passBundle
+
         transaction.replace(R.id.fragment_holder, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
@@ -55,7 +60,7 @@ class GameActivity : AppCompatActivity() {
     }
 
     //TO DO alert (when click you twice to back button you exit game
-   // val TAG=""
+    // val TAG=""
     //val twice:Boolean = false
     override fun onBackPressed() {
        //super.onBackPressed()
@@ -77,7 +82,6 @@ class GameActivity : AppCompatActivity() {
                twice=false;
 
            }
-       },3000)
-*/
+       },3000)*/
     }
 }
--- Mobile/app/src/main/java/pwr/damodarlepski/integrationgame/SummaryFragment.kt
+++ Mobile/app/src/main/java/pwr/damodarlepski/integrationgame/SummaryFragment.kt
@@ -1,6 +1,5 @@
 package pwr.damodarlepski.integrationgame
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -10,76 +9,45 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import pwr.damodarlepski.integrationgame.R.id.*
 
 class SummaryFragment : Fragment() {
 
     val TAG = "FragmentgameSummary"
-
-    override fun onAttach(context: Context?) {
-        Log.d(TAG,"onAttach")
-        super.onAttach(context)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG,"onCreate")
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d(TAG,"onCreateView")
+        val getBundle = arguments
+        val gameMechanics = getBundle?.getSerializable("game_mechanics") as GameMechanics
 
         val view = inflater.inflate(R.layout.fragment_summary, container, false)
         val timeViewOne = view.findViewById(R.id.points_team_one_text) as TextView
-        timeViewOne.text = teamOneCounter.toString()
+        timeViewOne.text = gameMechanics.teamOneScore.toString()
         val timeViewTwo = view.findViewById(R.id.points_team_two_text) as TextView
-        timeViewTwo.text = teamTwoCounter.toString()
+        timeViewTwo.text = gameMechanics.teamTwoScore.toString()
 
         val button = view.findViewById(R.id.button_end_game) as Button
         button.setOnClickListener {
-            val act = Intent(view.getContext(), MainMenuActivity::class.java)
-            activity!!.startActivity(act)
+
+            if (gameMechanics.currentRound < gameMechanics.selectedRounds.size - 1) {
+                val transaction = fragmentManager?.beginTransaction()
+                val fragment = RoundFragment()
+
+                val passBundle = Bundle()
+                passBundle.putSerializable("game_mechanics", gameMechanics)
+                fragment.arguments = passBundle
+
+                transaction?.replace(R.id.fragment_holder, fragment)
+                transaction?.addToBackStack(null)
+                transaction?.commit()
+            } else {
+
+                val act = Intent(view.context, MainMenuActivity::class.java)
+                activity!!.startActivity(act)
+            }
         }
         return view
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.d(TAG,"onActivityCreated")
-        super.onActivityCreated(savedInstanceState)
-    }
-
-    override fun onStart() {
-        Log.d(TAG,"onStart")
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d(TAG,"onResume")
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d(TAG,"onPause")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG,"onStop")
-        super.onStop()
-    }
-
-    override fun onDestroyView() {
-        Log.d(TAG,"onDestroyView")
-        super.onDestroyView()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG,"onDestroy")
-        super.onDestroy()
-    }
-
-    override fun onDetach() {
-        Log.d(TAG,"onDetach")
-        super.onDetach()
     }
 }
