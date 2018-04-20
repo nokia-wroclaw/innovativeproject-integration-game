3d88488a3665bce237e1dad94585df78e9444676
emlk0404 <36955409+emlk0404@users.noreply.github.com>
2018-04-20 15:34:22 +0200

    Add files via upload

---
+++ Backend/API/server/controllers/people.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var people = sequelize.define('people', {
+    name: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    nickname: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  people.associate = models => {
+    people.belongsTo(models.category, 
+      {
+        foreignKey: 'categoryId',
+        onDelete: 'CASCADE'
+    });
+  };
+  return people;
+};
