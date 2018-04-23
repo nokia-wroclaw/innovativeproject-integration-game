6f8fbe1598f5001193cf2fb1c10732f53646cb56
emil <emlk0404@gmail.com>
2018-04-23 19:52:50 +0200

    add cors, third try

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const cors = require('cors');
 const app = express();
 
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+ // res.header('Access-Control-Allow-Headers', 'Content-Type');
+//  next();
+
+  res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
 });
 
 app.use(logger('dev'));
