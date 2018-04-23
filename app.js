6222fee5b957fd2f678368d7cd211972a14487e4
emil <emlk0404@gmail.com>
2018-04-23 17:46:06 +0200

    Add randlist route

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
-  message: 'POST: \n/api/categories - post new category\n/api/categories/:categoryId/people - post new character in category with <:categoryId> id\n\n'
-  ,message: 'GET: \n/api/categories - list all categories\n/api/categories/:categoryId - retrieve one category (with all people belonging to it) with <:categoryId> id\n'
-  ,message: '/api/categories/random/size/:size/id/:id - retrieve <:size> number of category with <:id> id\n',
-  message: '/api/people - list all people\n/api/people/:peopleId - retrieve one character with <:peopleId> id',
-  message: '\n\nPUT:\n/api/categories/:categoryId - update category with <:categoryId> id\n',
-  message: '/api/categories/:categoryId/people/:peopleId - update character with <:peopleId> id inside category with <:categoryId> id',
-  message: '\n\nDELETE\n/api/categories/:categoryId - delete category with <:categoryId> id\n/api/categories/:categoryId/people/:peopleId - delete character with <:peopleId> id belonging to category with <:categoryId> id'
+  message: 'Endpoint not found'
 }));
 
 module.exports = app;
